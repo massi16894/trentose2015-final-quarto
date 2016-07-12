c747b269026f2a9bc3a5023c9c35988e2e85e6ae
massi16894 <massimiliano_pizzolato@hotmail.it>
2016-07-12 15:36:08 +0200

    Sprint 1

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * MonsterModel provides basic methods to iterate
  * over a collection of "monsters"  
  */
+var count = 0;
 var MonsterModel = {
   
    /* Collection of available monsters */
@@ -10,6 +11,7 @@ var MonsterModel = {
    /* Start the iteration over the collection of monsters.
    it sets the first element as the current monster */    
    init : function() {
+       $("h2").append(tmpl.replace("IMG",MonsterModel.collection[0].name));
    },
   
    /*   Returns the current monster and sets as current monster
@@ -17,6 +19,13 @@ var MonsterModel = {
     * If no more monsters are available, it returns null
     */
    getCurrentMonster : function () { 
+       if(count<= MonsterModel.collection.length){
+            return MonsterModel.collection[count].name;
+       }
+       else{
+        return false;
+       }
+       
    },
   
    /* Checks if the name of the current monster 
@@ -24,6 +33,55 @@ var MonsterModel = {
     * correctly named the monster, false otherwise
     */
    catchMonster : function (monsterName){
+       
+       var name = MonsterModel.getCurrentMonster();
+       
+       console.log("NAME: "+name)
+       
+       if(name == monsterName){
+        return true;
+       }
+       else{
+        return false;
+       }
    }  
            
 };
+
+var tmpl ='<img id="monsterPicture" src="img/IMG.png">';
+
+$(document).ready(function(){  
+    
+    MonsterModel.init();
+    
+    $("button").on("click", function(){
+        
+        var name = $("input").val();
+        
+        console.log(name);
+        
+        var att = MonsterModel.catchMonster(name);
+        
+        console.log(att);
+        
+        if(att){
+            $("img").remove();
+            count++;
+            alert("Catched!!");
+            if(count == MonsterModel.collection.length){
+            alert("NO MORE MONSTERS!!!!!");
+        }
+            $("h2").append(tmpl.replace("IMG",MonsterModel.collection[count].name));
+
+            
+        }
+        else
+        {
+            alert("Sorry, master");
+        }
+    
+    });
+    
+    
+  
+});
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,10 +1,26 @@
 /* Remember that blanket will only work with brackets live preview */
 /* Try to maximise the coverage of the ParlaModel object */
 
-describe("Parla", function() {
+describe("Monster", function() {
 
-  it("should...", function() {  
+  describe(".catchMonster", function(){
+        it("return catchMonster", function(){
+            var value1 = MonsterModel.catchMonster("bulbasaur");
+            var value2 = MonsterModel.catchMonster("ciao");
+            expect(value1).toBe(true);
+            expect(value2).toBe(false);
+
+        });
+      
+    });
     
-  });    
+    describe(".getCurrentMonster", function(){
+        it("return current monster", function(){
+            var value1 = MonsterModel.getCurrentMonster();
+            expect(value1).toBe("bulbasaur");
+
+        });
+      
+    }); 
  
 });
